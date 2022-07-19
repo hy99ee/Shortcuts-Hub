e1e32510b4890da76a884c2c6b074ac45f8e1aa2
hy99ee <hy99ee@gmail.com>
2022-07-19 21:52:07 +0300

    Initial Commit

---
+++ LuckyLunch/LuckyLunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyLunchApp.swift
+//  LuckyLunch
+//
+//  Created by Hy99ee on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct LuckyLunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
